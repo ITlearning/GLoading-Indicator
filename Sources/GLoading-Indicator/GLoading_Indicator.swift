2ee9989fe6a0748d4c91b092b01bcfbf073e165b
yo7504 <yo7504@naver.com>
2023-02-19 17:10:40 +0900

    Chore: iOS 버전 세팅, SwiftUI 에서 사용할 수 있게 설정

--- Sources/GLoading-Indicator/GLoading_Indicator.swift
+++ Sources/GLoading-Indicator/GLoading_Indicator.swift
@@ -1,19 +1,50 @@
+import UIKit
+import SwiftUI
+
+public struct Global_IndicatorSwiftUI: UIViewRepresentable {
+        
+    static var shared = Global_IndicatorSwiftUI()
+    
+    var view = Global_Indicator()
+    
+    func show() {
+        view.showIndicator()
+    }
+    
+    func hide() {
+        view.hideIndicator()
+    }
+    
+//
+//    func showButton() {
+//        view.showButton()
+//    }
+    
+    public func makeUIView(context: Context) -> some UIView {
+        return view
+    }
+    
+    public func updateUIView(_ uiView: UIViewType, context: Context) {
+    }
+}
+
+@available(iOS 13.0.0, *)
 public struct GLoading_Indicator {
     //public private(set) var text = "Hello, World!"
     
     static var instance = GLoading_Indicator()
     
-    public var indicatorView = Global_Indicator()
+    public var indicatorView = Global_IndicatorSwiftUI()
     
     public init() {
         
     }
     
     func show() {
-        indicatorView.showIndicator()
+        indicatorView.show()
     }
     
     func hide() {
-        indicatorView.hideIndicator()
+        indicatorView.hide()
     }
 }
